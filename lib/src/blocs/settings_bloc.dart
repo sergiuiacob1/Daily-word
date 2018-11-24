c46fd187c6dde6effd5614cef0e6d1410f14ca52
sergiuiacob1 <sergiu.iacob1@gmail.com>
2018-11-24 22:15:41 +0200

    added settings language selector

--- lib/src/blocs/settings_bloc.dart
+++ lib/src/blocs/settings_bloc.dart
@@ -1,1 +1,40 @@
-class SettingsBloc {}
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+
+class SettingsBloc {
+  PublishSubject languagesStreamController = PublishSubject();
+  SharedPreferences _pref;
+  List<String> _selectedLanguages;
+
+  SettingsBloc() {
+    _selectedLanguages = [];
+    _buildSelectedLanguages();
+  }
+
+  void dispose() {
+    languagesStreamController.close();
+  }
+
+  void _buildSelectedLanguages() async {
+    _pref = await SharedPreferences.getInstance();
+    _selectedLanguages = _pref.getStringList("SelectedLanguages") ?? [];
+  }
+
+  void languageCheck(String languageName, bool value) async {
+    if (value)
+      _selectedLanguages.add(languageName);
+    else
+      _selectedLanguages.removeWhere((item) => item == languageName);
+
+    if (_pref != null) {
+      await _pref.setStringList("SelectedLanguages", _selectedLanguages);
+      languagesStreamController.add(_selectedLanguages);
+    }
+  }
+
+  Stream get languagesStream =>
+      languagesStreamController.stream.asBroadcastStream();
+
+  List<String> get selectedLanguages => _selectedLanguages;
+}
--- lib/src/ui/settings_ui.dart
+++ lib/src/ui/settings_ui.dart
@@ -1,112 +1,55 @@
 import 'package:flutter/material.dart';
 import 'page_utils.dart' as PageUtils;
+import 'package:shared_preferences/shared_preferences.dart';
 import '../blocs/settings_bloc.dart';
 import '../providers/settings_provider.dart';
+import './../models/language.dart';
 
 class SettingsUI extends StatelessWidget {
-  final title = "SettingsUI";
+  final title = "Settings";
 
   @override
   Widget build(BuildContext context) {
-    SettingsBloc settingsBloc = SettingsProvider.of(context);
+    SettingsBloc bloc = SettingsProvider.of(context);
     return new CustomScrollView(
       scrollDirection: Axis.vertical,
       slivers: <Widget>[
         PageUtils.buildSliverAppBar(title),
-        _buildContent(settingsBloc),
+        _buildContent(bloc),
       ],
     );
   }
 
-  Widget _buildContent(SettingsBloc settingsBloc) {
-    return SliverFillRemaining(
-      child: Container(
-        color: Colors.red[100],
+  Widget _buildContent(SettingsBloc bloc) {
+    return new SliverList(
+      delegate: new SliverChildListDelegate(
+        <Widget>[
+          _buildLanguageSelector(bloc),
+        ],
       ),
     );
   }
+
+  Widget _buildLanguageSelector(SettingsBloc bloc) {
+    return StreamBuilder(
+        stream: bloc.languagesStream,
+        initialData: bloc.selectedLanguages,
+        builder: (context, snapshot) {
+          // if (snapshot.hasData) {
+          //   print('I got data: ${snapshot.data}');
+          // } else
+          //   print('I dont have data');
+          return ExpansionTile(
+            title: new Text("Language select"),
+            children: languages.values
+                .map((language) => CheckboxListTile(
+                      title: Text(language.name),
+                      value: snapshot.data.contains(language.name),
+                      onChanged: (value) =>
+                          bloc.languageCheck(language.name, value),
+                    ))
+                .toList(),
+          );
+        });
+  }
 }
-
-// class Settings extends StatefulWidget {
-//   final String title = "Settings";
-
-//   Settings({Key key}) : super(key: key);
-
-//   @override
-//   State<StatefulWidget> createState() {
-//     return new _SettingsState();
-//   }
-// }
-
-// class _SettingsState extends State<Settings> {
-//   SharedPreferences pref;
-//   List<String> selectedLanguages;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     selectedLanguages = [];
-//     _buildSelectedLanguages();
-//     print('Creating: $widget.title');
-//   }
-
-//   void _buildSelectedLanguages() async {
-//     pref = await SharedPreferences.getInstance();
-//     setState(() {
-//       selectedLanguages = pref.getStringList("SelectedLanguages") ?? [];
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print('Build: $widget.title');
-//     return new CustomScrollView(
-//       scrollDirection: Axis.vertical,
-//       slivers: <Widget>[
-//         PageUtils.buildSliverAppBar(widget.title),
-//         _buildContent(),
-//       ],
-//     );
-//   }
-
-//   Widget _buildContent() {
-//     return new SliverList(
-//       delegate: new SliverChildListDelegate(
-//         <Widget>[
-//           _buildLanguageSelector(),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _buildLanguageSelector() {
-//     return ExpansionTile(
-//       title: new Text("Language select"),
-//       children: languages.values
-//           .map((language) => CheckboxListTile(
-//                 title: Text(language.name),
-//                 value: _isLanguageSelected(language.name),
-//                 onChanged: (value) => _languageCheck(language.name, value),
-//               ))
-//           .toList(),
-//     );
-//   }
-
-//   void _languageCheck(String languageName, bool value) async {
-//     print('Press $languageName: $value');
-//     if (value)
-//       selectedLanguages.add(languageName);
-//     else
-//       selectedLanguages.remove(languageName);
-
-//     if (pref != null) {
-//       await pref.setStringList("SelectedLanguages", selectedLanguages);
-//       setState(() {});
-//     }
-//   }
-
-//   bool _isLanguageSelected(String _languageName) {
-//     print('Vector: $selectedLanguages');
-//     return selectedLanguages.contains(_languageName);
-//   }
-// }
